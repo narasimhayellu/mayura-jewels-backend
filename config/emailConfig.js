0f8e3b29cedd7a96c73b1a87946b7b26db998ec7
narasimhayellu <narasimhayellu99@gmail.com>
2026-05-18 14:56:21 +0530

    nodemailer

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -1,14 +1,20 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    family: 4,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  connectionTimeout: 10000,
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP ERROR:", error);
+  } else {
+    console.log("Email server ready");
+  }
+});
 
 module.exports = transporter;
