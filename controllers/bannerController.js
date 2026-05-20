836b03f2f1a8a7dc2b53735d5d870abf7c751652
narasimhayellu <narasimhayellu99@gmail.com>
2026-05-20 11:59:30 +0530

    title

--- controllers/bannerController.js
+++ controllers/bannerController.js
@@ -4,14 +4,20 @@ const getBanner = async(req,res)=>{
             {
               image:
                 "https://i.ytimg.com/vi/3x6x9eXmj3U/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDokbTFAVwb9lituX8sSZ4D2wOQwg",
+              title:"Necklace",
+              id:1  
             },
             {
               image:
                 "https://www.londongold.com/media/uploads/Lab%20Grown%20Certs/Tennis-lab_banner.jpg",
+                title:"Bangles",
+                id:2  
             },
             {
               image:
                 "https://images.bhimagold.com/admin/category/images/1778164361203-earrings-1200x600.jpg",
+                title:"Earrings",
+                id:3  
             },
           ];
 
